7627d5ce9cb8f8d702600663b9a5d25b0146e757
xseed-yogesh <yogesh.sharma@edpulsed.com>
2021-06-20 01:11:06 +0600

    model change

--- lib/Models/Payment/purchase.dart
+++ lib/Models/Payment/purchase.dart
@@ -1,31 +1,17 @@
 class Purchase {
   String dateTime;
-  int purchaseAmount;
-  String paymentMethodType;
-  int eventId;
   int id;
 
-  Purchase(
-      {this.dateTime,
-      this.purchaseAmount,
-      this.paymentMethodType,
-      this.eventId,
-      this.id});
+  Purchase({this.dateTime, this.id});
 
   Purchase.fromJson(Map<String, dynamic> json) {
     dateTime = json['dateTime'];
-    purchaseAmount = json['purchaseAmount'];
-    paymentMethodType = json['paymentMethodType'];
-    eventId = json['eventId'];
     id = json['id'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['dateTime'] = this.dateTime;
-    data['purchaseAmount'] = this.purchaseAmount;
-    data['paymentMethodType'] = this.paymentMethodType;
-    data['eventId'] = this.eventId;
     data['id'] = this.id;
     return data;
   }
